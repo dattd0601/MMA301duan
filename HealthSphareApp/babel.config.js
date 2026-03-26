492dd9c50c5ca7a016878063d1944f05d32c55ae
TranDinhDat <dattdhe186026@fpt.edu.vn>
2026-03-26 10:31:37 +0700

    fix: resolve babel worklets plugin

--- HealthSphareApp/babel.config.js
+++ HealthSphareApp/babel.config.js
@@ -1,13 +1,10 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-worklets-core/plugin",
-      "react-native-reanimated/plugin",
+      'react-native-worklets-core/plugin', 
+      'react-native-reanimated/plugin', // PHẢI Ở CUỐI CÙNG
     ],
   };
 };
